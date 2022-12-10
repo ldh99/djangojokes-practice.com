fa1c2b22da07feb7ec6705bc0f3e22681e886063
Tony <hvn99@yahoo.com>
2022-12-10 13:47:27 -0800

    Create Joke model by add the class Joke to the jokes/models.py Run py manage.py makemigrations and py manage.py migrate to add Joke model to the database

--- jokes/models.py
+++ jokes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Joke(models.Model):
+  question = models.TextField(max_length=200)
+  answer = models.TextField(max_length=100, blank=True)
+  # auto_now_add=True - field will automatically be assigned 
+  # current date and time when the row is created
+  created = models.DateTimeField(auto_now_add=True)
+  # auto_now=True - value of field will be changed to current
+  # date and time when the row is updated
+  updated = models.DateTimeField(auto_now=True)
+
+  # __str__() method - return output of the row convert to a string
+  def __str__(self):
+    return self.question
